44adea872fd74b766b23213087e92b5f3589731b
brandononeal <baoneal95@gmail.com>
2020-09-11 10:11:34 -0700

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { BASE_URL } from "./constants";
+import Character from "./components/Character";
+import "./App.css";
+
+const StyledTitle = styled.div`
+  width: 85%;
+  margin: 0 auto;
+  h1 {
+    color: ${(pr) => pr.theme.primaryColor};
+    font-size: 2.5rem;
+  }
+`;
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [character, setCharacter] = useState([]);
+  // const [currentCharacter, setCurrentCharacter] = useState(null);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // const openCharacter = (id) => {
+  //   setCurrentCharacter(id);
+  // };
+
+  // const closeCharacter = (id) => {
+  //   setCurrentCharacter(null);
+  // };
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}`)
+      .then((res) => {
+        setCharacter(res.data.results);
+      })
+      .catch((err) => {
+        debugger;
+      });
+  }, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledTitle>
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+        {character.map((characterObject) => {
+          return <Character character={characterObject} />;
+        })}
+        {console.log(character)}
+      </div>
+    </StyledTitle>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledDetails = styled.div`
+  color: ${(pr) => pr.theme.secondaryColor};
+  h2 {
+    font-size: 2rem;
+  }
+  h3 {
+    font-size: 1.5rem;
+  }
+`;
+
+export default function Character(props) {
+  const { character } = props;
+
+  return (
+    <StyledDetails>
+      <div className="characters-container">
+        <img src={character.image} alt={character.name}></img>
+        <h2>Name: {character.name}</h2>
+        <h3>Status: {character.status}</h3>
+        <h3>Gender: {character.gender}</h3>
+        <h3>Species: {character.species}</h3>
+        <h3>Origin: {character.origin.name}</h3>
+        <h3>Location: {character.location.name}</h3>
+      </div>
+    </StyledDetails>
+  );
+}
